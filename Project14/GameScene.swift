871febb39896ceaf286c41ea28a8efe17d158422
jslusser <jslusser@gmail.com>
2017-06-12 17:44:55 +0300

    Initial commit

---
+++ Project14/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by James Slusser on 6/12/17.
+//  Copyright © 2017 James Slusser. All rights reserved.
+//  https://www.hackingwithswift.com/read/14/overview
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+}
